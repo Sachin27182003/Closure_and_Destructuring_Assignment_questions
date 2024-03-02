b1f84810aa2fa23f8ad4240e7aec41959a8e7826
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2024-03-02 20:10:04 +0530

    New Products Added

--- shopping_cart.js
+++ shopping_cart.js
@@ -6,14 +6,27 @@ function shoppingCart (){
        name: "Fan",
        price: 4000,
        discountedPrice: 3000
-
    }
+
+   let Items = [];
     return {
         getCartItems: function(){
-         return product;
-    }}
+         return {product, Items};
+    },
+    addToCart: function(product){
+        Items.push(product)
+    }
+}
 }
 
 const cart = shoppingCart()
-//console.log(cart())
+
+//console.log("cart Items:", cart.getCartItems())
+
+const product1 = { id: 1, name: "product1", price: 10}
+const product2 = { id: 1, name: "product2", price: 10}
+
+cart.addToCart(product1)
+cart.addToCart(product2)
+
 console.log("cart Items:", cart.getCartItems())
