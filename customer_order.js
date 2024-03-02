06eafaac5fb1cc6770a5f42154a76355135c5606
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2024-03-02 19:34:08 +0530

    destructuring oder

---
+++ customer_order.js
@@ -0,0 +1,13 @@
+
+
+let order = {
+    orderId: "123456",
+    productName: "Laptop",
+    quantity: 2
+};
+
+let { orderId, productName, quantity } = order;
+
+console.log(orderId)
+console.log(productName)
+console.log(quantity)
